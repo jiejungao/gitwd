ac5f789060cc3f16117b3c01f2ee2c84abcecb84
高介军 <gaojiejun@gaojiejundeMacBook-Pro-2.local>
2017-02-03 12:33:23 +0800

    first file.

---
+++ creategit.sql
@@ -0,0 +1,18 @@
+
+-- 安装brew 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+-- 安装 git
+git install git
+
+-- 查看详细路径
+ brew list git
+
+-- brew list命令可以查看安装的开源软件包。
+brew list 
+
+
+-- 创建目录
+$ mkdir learngit
+--通过git init命令把这个目录变成Git可以管理的仓库：
+git init 
